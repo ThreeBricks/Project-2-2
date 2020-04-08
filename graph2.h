dba81f6c100b27a7780a7b418845a758a54c97e3
ThreeBricks <54003456+ThreeBricks@users.noreply.github.com>
2020-04-08 08:58:28 -0400

    Update

---
+++ graph2.h
@@ -0,0 +1,61 @@
+
+class Node{
+private:
+    int val;
+    mutable int inDegree;
+    mutable std::unordered_set<const Node*> neighbors;
+public:
+    Node(const int num){
+        val=num;
+        inDegree=0;
+    }
+
+    int getVal()const{
+        return val;
+    }
+    void addNeighbor(const Node* n)const{
+        if(n->getVal()!=val){
+           if(neighbors.insert(n).second){
+                n->inDegree+=1;
+           }
+        }
+    }
+    void removeNeighbor(const Node* n)const{
+        if(neighbors.find(n)!=neighbors.end()){
+            neighbors.erase(neighbors.find(n));
+            return;
+        }
+    }
+
+    bool operator==(const Node*n2){
+        if(val==n2->getVal()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    std::unordered_set<const Node*> getNeighbors()const{
+        return neighbors;
+    }
+    int getInDegree()const{
+        return inDegree;
+    }
+    void updateInDegree()const{
+        inDegree+=1;
+        return;
+    }
+    void decreaseInDegree()const{
+        inDegree-=1;
+        return;
+    }
+
+};
+
+class Graph{
+public:
+    virtual void addNode(const int& nodeVal)=0;
+    virtual std::unordered_set<Node*> getAllNodes()const=0;
+    virtual std::vector<Node*> getAdjList()const=0;
+};
+
